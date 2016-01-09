a0b4499ceb838536e622f32db73f51a511eaee0a
SantaKruse <dkruse22@live.co.uk>
2016-01-09 11:51:40 +0000

    db setup with game and game sessions

--- db/migrate/20160107222010_create_tables.rb
+++
@@ -1,9 +0,0 @@
-class CreateTables < ActiveRecord::Migration
-  def change
-    create_table :tables do |t|
-      t.string :title
-      t.string :giphy_url
-      t.boolean :posted_to_facebook
-    end
-  end
-end
