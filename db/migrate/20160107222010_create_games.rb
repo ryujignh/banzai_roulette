77991ef73b8e9462b67f45460a8989bb1c277b51
SantaKruse <dkruse22@live.co.uk>
2016-01-10 00:25:05 +0000

    New game submit form created

--- db/migrate/20160107222010_create_games.rb
+++ db/migrate/20160107222010_create_games.rb
@@ -2,7 +2,6 @@ class CreateGames < ActiveRecord::Migration
   def change
     create_table :games do |t|
       t.string :name
-      t.string :password
       t.integer :round_number
       t.boolean :round_open
       t.string :word
