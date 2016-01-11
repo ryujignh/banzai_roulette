64771585bcae9d7aedfd4675cd50f7777c9b3037
Ryuji Ganaha <RyujiGanaha@MacBook-Pro.local>
2016-01-11 12:01:00 -0800

    fixed signout

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root 'top#index', via: :get
   get 'auth/:provider/callback', to: "sessions#create"
-  delete 'sign_out', to: "sessions#destroy", as: 'sign_out'
+  get 'sign_out', to: "sessions#destroy", as: 'sign_out'
   get 'post_to_wall', to: "users#post_to_wall"
   # get 'auth/facebook', as: "auth_provider"
   # get 'auth/facebook/callback', to: 'users#login'
