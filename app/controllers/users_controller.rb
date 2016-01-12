4070368abdd080a98a5ed29ae33fc678c409ea2a
SantaKruse <dkruse22@live.co.uk>
2016-01-12 16:53:18 +0000

    your games functionality ammended to use gamesessions not just game

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,9 +6,10 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @games = @user.games
+    @gamesessions = Gamesession.select("users.first_name, users.last_name, gamesessions.*")
+    .joins("LEFT OUTER JOIN users ON users.id = gamesessions.user_id")
+    .where("gamesessions.user_id = #{current_user.id} and joined = true")
     @gameinvites = Gamesession.where("user_id = #{current_user.id} and joined is null")
   end
 
 end
-
-
