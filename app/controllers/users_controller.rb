ae6c77a372ea4fab4110066cda5997c1b7b1fe29
SantaKruse <dkruse22@live.co.uk>
2016-01-12 17:21:01 +0000

    game owner sql corrected

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,8 +6,9 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @games = @user.games
-    @gamesessions = Gamesession.select("users.first_name, users.last_name, gamesessions.*")
-    .joins("LEFT OUTER JOIN users ON users.id = gamesessions.user_id")
+    @gamesessions = Gamesession.select("users.first_name, users.last_name, gamesessions.*, games.*")
+    .joins("LEFT OUTER JOIN games ON games.id = gamesessions.game_id")
+    .joins("LEFT OUTER JOIN users ON games.user_id = users.id")
     .where("gamesessions.user_id = #{current_user.id} and joined = true")
     @gameinvites = Gamesession.where("user_id = #{current_user.id} and joined is null")
   end
