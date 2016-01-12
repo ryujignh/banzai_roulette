dc32d94b768436d273306899484241e952b5e786
CLarmour <connorlarmour@outlook.com>
2016-01-12 13:08:17 -0800

    updated giphy files

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,37 +12,37 @@
 //
 //= require_tree .
 
-function create_player_circle(no_players) {
-  var x_center = $(".center_marker").position().left;
-  var y_center = $(".center_marker").position().top;
-  var items = no_players;
-  var r = x_center/2
-  for(var i = 0; i < items; i++) {
-    var x = x_center + r * Math.cos(2 * Math.PI * i / items);
-    var y = y_center + r * Math.sin(2 * Math.PI * i / items);   
-    var player_outer_wrapper = $("<div/>",{"class":"player_outer_wrapper", "id":i});
-    $(player_outer_wrapper).css("top", y + "px").css("left", x + "px");;
-    var player_wrapper = $("<div/>",{"class":"player_wrapper"});
-    var player_caption = $("<p/>", {"text":'Join', "class":"player_caption"});
-    $(player_outer_wrapper).append(player_wrapper).append(player_caption);
-    $("#player_circle").append(player_outer_wrapper);
-  };
-};
+// function create_player_circle(no_players) {
+//   var x_center = $(".center_marker").position().left;
+//   var y_center = $(".center_marker").position().top;
+//   var items = no_players;
+//   var r = x_center/2
+//   for(var i = 0; i < items; i++) {
+//     var x = x_center + r * Math.cos(2 * Math.PI * i / items);
+//     var y = y_center + r * Math.sin(2 * Math.PI * i / items);   
+//     var player_outer_wrapper = $("<div/>",{"class":"player_outer_wrapper", "id":i});
+//     $(player_outer_wrapper).css("top", y + "px").css("left", x + "px");;
+//     var player_wrapper = $("<div/>",{"class":"player_wrapper"});
+//     var player_caption = $("<p/>", {"text":'Join', "class":"player_caption"});
+//     $(player_outer_wrapper).append(player_wrapper).append(player_caption);
+//     $("#player_circle").append(player_outer_wrapper);
+//   };
+// };
 
-function load_player_circle(uid,id,current_user_id, player_no) {
-  var player_outer_wrapper  = $("#" + player_no)
-  var player_wrapper = $(player_outer_wrapper).find(".player_wrapper"); 
-  var player_img = $("<img/>", {"src":"https://graph.facebook.com/" + uid + "/picture?type=large", "class":"player_img"});
-  $(player_wrapper).append(player_img)
+// function load_player_circle(uid,id,current_user_id, player_no) {
+//   var player_outer_wrapper  = $("#" + player_no)
+//   var player_wrapper = $(player_outer_wrapper).find(".player_wrapper"); 
+//   var player_img = $("<img/>", {"src":"https://graph.facebook.com/" + uid + "/picture?type=large", "class":"player_img"});
+//   $(player_wrapper).append(player_img)
 
-};
+// };
 
-  $(window).bind('resize', function(e)
-  {
-    console.log('window resized..');
-    $( ".player_outer_wrapper" ).remove();
-    player_circle()
-  });
+//   $(window).bind('resize', function(e)
+//   {
+//     console.log('window resized..');
+//     $( ".player_outer_wrapper" ).remove();
+//     player_circle()
+//   });
 
   //setTimeout(function () {
   //  window.location = window.location
