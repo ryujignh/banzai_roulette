b0b2834969c9c2e39d8f8347516ac640948e80a5
Ryuji Ganaha <RyujiGanaha@MacBook-Pro-2.local>
2016-01-21 14:19:16 -0800

    installed new gem

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,7 @@
-
-
-10.times do
-  Gamesession.create(
-    user_id: FFaker::numerify,
-    game_id:
-    word: FFaker::Movie.title,
-  )
-end
+# 10.times do
+#   Gamesession.create(
+#     user_id: FFaker::numerify,
+#     game_id:
+#     word: FFaker::Movie.title,
+#   )
+# end
