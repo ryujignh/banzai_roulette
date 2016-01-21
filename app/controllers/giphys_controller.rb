f794e2747395596a722dd8b7093c864d22419266
Ryuji Ganaha <RyujiGanaha@MacBook-Pro-2.local>
2016-01-20 17:41:53 -0800

    modified top/index and giphy/form and its css

--- app/controllers/giphys_controller.rb
+++ app/controllers/giphys_controller.rb
@@ -2,7 +2,10 @@ class GiphysController < ApplicationController
   skip_before_action :verify_authenticity_token
   def create
     @user = current_user
-    # u = current_user
     @user.facebook.put_wall_post("", {"link" => params[:gifurl]})
+  end
+
+  def show
+    
   end
 end
