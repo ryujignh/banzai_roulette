6b2915289cda3f1f2db95e0172f58c3195daee3f
Ryuji Ganaha <RyujiGanaha@MacBook-Pro-2.local>
2016-01-21 14:49:40 -0800

    Removed wins column from Gamesessions tabale

--- config/routes.rb
+++ config/routes.rb
@@ -1,27 +1,17 @@
 Rails.application.routes.draw do
-  get 'words/new'
 
   root 'top#index', via: :get
 
   post 'top/authorize'
   post '/', controller: :top, action: :index
-  # post "games/post_giphy"
 
   get 'auth/:provider/callback', to: "sessions#create"
   get 'sign_out', to: "sessions#destroy", as: 'sign_out'
   get 'post_to_wall', to: "users#post_to_wall"
-  # get '/contacts' do
-  #   Contact.all.to_json
-  # end
-
-
-  # get 'auth/facebook', as: "auth_provider"
-  # get 'auth/facebook/callback', to: 'users#login'
 
   resources :users
   resources :games, only: [:index, :create, :show, :update, :post_giphy]
   resources :gamesessions
-  resources :words, only: [:index, :create, :show]
   resources :top
   resources :invites, only: [:create]
   resources :giphys
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160109234158) do
+ActiveRecord::Schema.define(version: 20160121224734) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,7 +31,6 @@ ActiveRecord::Schema.define(version: 20160109234158) do
     t.integer "user_id"
     t.integer "game_id"
     t.string  "word"
-    t.integer "wins"
     t.integer "loses"
     t.boolean "active"
     t.boolean "joined"
